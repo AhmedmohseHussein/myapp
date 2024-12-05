1ac631417035a21f7aa45bdfc445abcf21245a14
am3463485 <am3463485@gmail.com>
2024-12-05 18:39:12 +0000

    project Initializing

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Groceries',
+      theme: ThemeData.dark().copyWith(
+      
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 42, 51, 59),
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+      ),
+      home: Center(
+        
+        child: Container(
+            color: Colors.red,
+           padding:const EdgeInsets.all(50),
+            child: Container(
+              
+              color: Colors.green,
+              width: 40,
+              height: 40,
+            ),
+           
+            
+          ),
+      ),
+
+        
+    
+    );
+  }
+}
